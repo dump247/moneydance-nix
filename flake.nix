57f243fe3a7d10c4edf015794894abcd0833b0d7
Cory Thomas <cthomas7577@gmail.com>
2022-09-03 18:12:47 -0500

    Output a single package
    Remove the overlay and the list of system names since we are only building a single system.
    Not sure how the overlay would be useful for this flake.

--- flake.nix
+++ flake.nix
@@ -6,40 +6,24 @@
 
   outputs = { self, nixpkgs }:
     let
-
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-
-      # Version and sha256 of the Moneydance linux tarball
       version = "2022.5_4091";
-      sha256 = "6bc9d9c4ad3b3a5368d79f8374991e6af75bb4038721c6c2f17506b185e6d560";
-      moneydanceUrl = "https://infinitekind.com/stabledl/${version}/moneydance-linux.tar.gz";
+      versionUrl = "https://infinitekind.com/stabledl/${version}";
 
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlays.default ]; });
-
+      linux = {
+        sha256 = "6bc9d9c4ad3b3a5368d79f8374991e6af75bb4038721c6c2f17506b185e6d560";
+        url = "${versionUrl}/moneydance-linux.tar.gz";
+      };
     in
-
     {
+      packages.x86_64-linux.default = with nixpkgs; stdenv.mkDerivation rec {
+        name = "moneydance-${version}";
 
-      # A Nixpkgs overlay.
-      overlays.default = final: prev: {
+        src = fetchurl {
+          inherit (linux) sha256 url;
+        };
 
-        moneydance = with final; stdenv.mkDerivation rec {
-          name = "moneydance-${version}";
-
-          src = fetchurl {
-            url = moneydanceUrl;
-            inherit sha256;
-          };
-
-          buildCommand = let
+        buildCommand =
+          let
             jreLibPath = lib.makeLibraryPath [
               zlib
               xorg.libXext
@@ -50,7 +34,8 @@
               freetype
               fontconfig
             ];
-          in ''
+          in
+          ''
           # Unpack the tarball, removing the leading "moneydance/" directory.
           mkdir -p "$out"
           tar xfvz "$src" --strip-components=1 -C "$out"
@@ -65,15 +50,6 @@
             --set-rpath "$out/jre/lib:${jreLibPath}" \
             "$out"/jre/lib/*.so
           '';
-        };
-
       };
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        {
-          inherit (nixpkgsFor.${system}) moneydance;
-          default = nixpkgsFor.${system}.moneydance;
-        });
     };
 }
