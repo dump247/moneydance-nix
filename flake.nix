1da418408a75a95f42126530b8990cdc38965bcf
Cory Thomas <cthomas7577@gmail.com>
2023-05-21 22:09:49 -0500

    Refactored the flake definition
    Simplified the flake definition. Removed unnecessary function calls and
    an unnecessary overlay definition.

--- flake.nix
+++ flake.nix
@@ -5,75 +5,45 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
 
   outputs = { self, nixpkgs }:
-    let
+  let
+    # Version and sha256 of the Moneydance linux tarball
+    version = "2023.1_5006";
+    sha256 = "e8fe0d3941b35ba9230bcfc49127d6940230da03b16392d11ecc14a1c8ffc521";
+    url = "https://infinitekind.com/stabledl/${version}/moneydance-linux.tar.gz";
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in
+  {
+    packages.x86_64-linux.default = with pkgs; stdenv.mkDerivation rec {
+      name = "moneydance-${version}";
 
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      src = fetchurl { inherit sha256 url; };
 
-      # Version and sha256 of the Moneydance linux tarball
-      version = "2023.1_5006";
-      sha256 = "e8fe0d3941b35ba9230bcfc49127d6940230da03b16392d11ecc14a1c8ffc521";
-      moneydanceUrl = "https://infinitekind.com/stabledl/${version}/moneydance-linux.tar.gz";
+      buildCommand = let
+        jreLibPath = lib.makeLibraryPath [
+          zlib
+          xorg.libXext
+          xorg.libXtst
+          xorg.libX11
+          xorg.libXrender
+          xorg.libXi
+          freetype
+          fontconfig
+        ];
+      in ''
+      # Unpack the tarball, removing the leading "moneydance/" directory.
+      mkdir -p "$out"
+      tar xfvz "$src" --strip-components=1 -C "$out"
 
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" ];
+      # Patch the Java interpreter load library paths
+      patchelf \
+        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        --set-rpath "$out/jre/lib:${jreLibPath}" \
+        "$out"/jre/bin/*
 
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlays.default ]; });
-
-    in
-
-    {
-
-      # A Nixpkgs overlay.
-      overlays.default = final: prev: {
-
-        moneydance = with final; stdenv.mkDerivation rec {
-          name = "moneydance-${version}";
-
-          src = fetchurl {
-            url = moneydanceUrl;
-            inherit sha256;
-          };
-
-          buildCommand = let
-            jreLibPath = lib.makeLibraryPath [
-              zlib
-              xorg.libXext
-              xorg.libXtst
-              xorg.libX11
-              xorg.libXrender
-              xorg.libXi
-              freetype
-              fontconfig
-            ];
-          in ''
-          # Unpack the tarball, removing the leading "moneydance/" directory.
-          mkdir -p "$out"
-          tar xfvz "$src" --strip-components=1 -C "$out"
-
-          # Patch the Java interpreter load library paths
-          patchelf \
-            --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-            --set-rpath "$out/jre/lib:${jreLibPath}" \
-            "$out"/jre/bin/*
-
-          patchelf \
-            --set-rpath "$out/jre/lib:${jreLibPath}" \
-            "$out"/jre/lib/*.so
-          '';
-        };
-
-      };
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        {
-          inherit (nixpkgsFor.${system}) moneydance;
-          default = nixpkgsFor.${system}.moneydance;
-        });
+      patchelf \
+        --set-rpath "$out/jre/lib:${jreLibPath}" \
+        "$out"/jre/lib/*.so
+      '';
     };
+  };
 }
